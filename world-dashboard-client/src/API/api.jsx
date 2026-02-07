4866d4392a51b48035aa1f5188cd4e08c331738a
Chandangadewar <144953053+Chandangadewar@users.noreply.github.com>
2026-02-07 13:39:37 +0530

    Update api.jsx

--- world-dashboard-client/src/API/api.jsx
+++ world-dashboard-client/src/API/api.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// Use environment variable or fallback to localhost
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000/dashboard/";
+// Use PUBLIC EC2 BACKEND URL
+const BASE_URL = "http://13.232.90.46:8000/dashboard";
 
 const api = axios.create({ baseURL: BASE_URL });
 
-export const apiRequest = async (endpoint, data = {}, method = 'get') => {
+export const apiRequest = async (endpoint, data = {}, method = "get") => {
     try {
         const response = await api.request({
             url: endpoint,
@@ -20,23 +20,24 @@ export const apiRequest = async (endpoint, data = {}, method = 'get') => {
 };
 
 // total population
-export const totalPopulation = async () => await apiRequest('/total-population');
+export const totalPopulation = async () => await apiRequest("/total-population");
 
 // total countries
-export const totalCountries = async () => await apiRequest('/total-countries');
+export const totalCountries = async () => await apiRequest("/total-countries");
 
 // Top10PopulatedCountries
-export const getTop10PopulatedCountries = async () => await apiRequest('/top-10-populated-countries');
+export const getTop10PopulatedCountries = async () => await apiRequest("/top-10-populated-countries");
 
-export const getTop10LeastPopulatedCountries = async () => await apiRequest('/top-10-least-populated-countries');
+export const getTop10LeastPopulatedCountries = async () =>
+    await apiRequest("/top-10-least-populated-countries");
 
-export const getAveragePopulation = async () => await apiRequest('/average-population');
+export const getAveragePopulation = async () => await apiRequest("/average-population");
 
-export const getTotalPopulationByContinent = async () => await apiRequest('/total-population-by-continent');
+export const getTotalPopulationByContinent = async () =>
+    await apiRequest("/total-population-by-continent");
 
+export const getTotalLanguages = async () => await apiRequest("/total-languages");
 
-export const getTotalLanguages = async () => await apiRequest('/total-languages');
+export const getTop10Languages = async () => await apiRequest("/top-10-languages");
 
-export const getTop10Languages = async () => await apiRequest('/top-10-languages');
-
-export const getLargestCities = async () => await apiRequest('/largest-cities');
+export const getLargestCities = async () => await apiRequest("/largest-cities");
